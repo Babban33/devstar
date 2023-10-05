a94a83a2884ef23ae4871d6d611d4a2e1c780b09
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-05 11:59:21 +0530

    add : added a form vairable

---
+++ src/routes/(tools)/resume-builder/TaglineCard/info.js
@@ -0,0 +1,6 @@
+let taglineDetails = {
+    tagline: "Crafting Code, Building Futures: Your Expert Software Developer!",
+    techSkills: ["Software Developer"],
+}
+
+export default taglineDetails;
