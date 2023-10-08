b06dfb68e8eaf6d675adb5c455ad17111156c8fe
bhumikadubey <bhoomidubey13@gmail.com>
2023-10-08 21:02:29 +0530

    update : updated the form

--- src/routes/(tools)/resume-builder/+page.svelte
+++ src/routes/(tools)/resume-builder/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-    import ProfileCard  from "./ProfileCard/ProfileCard.svelte";
-    import TaglineCard  from "./TaglineCard/TaglineCard.svelte";
-    import InformationCard  from "./InformationCard/InformationCard.svelte";
+    import ProfileCard from "./ProfileCard/ProfileCard.svelte";
+    import TaglineCard from "./TaglineCard/TaglineCard.svelte";
+    import InformationCard from "./InformationCard/InformationCard.svelte";
+    import TaglineDetails from "./TaglineCard/taglineDetails.svelte";
 </script>
 <div>
     <section class="bg-white dark:bg-gray-900">
@@ -14,7 +15,8 @@
             </p>
         </div>
     </section>
+    <InformationCard/>
     <ProfileCard/>
     <TaglineCard/>
-    <InformationCard/>
+    <TaglineDetails/>
 </div>
--- src/routes/(tools)/resume-builder/TaglineCard/taglineDetails.svelte
+++ src/routes/(tools)/resume-builder/TaglineCard/taglineDetails.svelte
@@ -1,11 +1,15 @@
 <script>
   import taglineDetails from "./info";
+  let isEditing = false
+  function toggleForm(){
+    isEditing = !isEditing;
+  }
 </script>
 <div class="bg-gray-900 pb-5 pl-5 pr-5">
     <div class="max-w-md rounded-2xl bg-gray-400 p-5 font-serif text-gray-900">
       <div class="flex items-center text-2xl justify-between font-semibold">
         <h5>Tagline</h5>
-        <button class="inline-flex items-center gap-2 rounded-2xl bg-gray-900 px-4 py-2 text-white font-medium">
+        <button class="inline-flex items-center gap-2 rounded-2xl bg-gray-900 px-4 py-2 text-white font-medium" on:click={toggleForm}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" /></svg
           >Edit
