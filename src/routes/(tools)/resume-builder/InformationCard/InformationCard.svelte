0af81bf26cf0f773694ba6051ae97966ab30c07b
PrachiZodage <prachizodage4@gmail.com>
2023-10-12 21:42:45 +0530

    update: added on submit prop

--- src/routes/(tools)/resume-builder/InformationCard/InformationCard.svelte
+++ src/routes/(tools)/resume-builder/InformationCard/InformationCard.svelte
@@ -4,6 +4,8 @@
     import selectedButton from "../ProfileCard/ProfileCard.svelte";
     import WorkExperienceMiddleTile from "./WorkExperienceMiddleTile.svelte";
 	import WorkExperienceTile from "./WorkExperienceTile.svelte";
+	import ProjectMiddleTile from "./ProjectMiddleTile.svelte";
+	import ProjectTextTile from "./ProjectTextTile.svelte";
 </script>
-<EducationFrame/>
-<CollegeTileText/>
+<ProjectMiddleTile/>
+<ProjectTextTile/>
