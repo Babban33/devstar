1d2e5d3c1a526d519b2b59770f8ff1b7ff2ca056
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-13 13:57:25 +0530

    update: used clickBtn store to render middletile and texttile of respective components

--- src/routes/(tools)/resume-builder/InformationCard/InformationCard.svelte
+++ src/routes/(tools)/resume-builder/InformationCard/InformationCard.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
+    import { clickedBtn } from "../ProfileCard/ClickedButton";
     import EducationFrame from "./EducationFrame.svelte";
     import CollegeTileText from "./CollegeTileText.svelte";
-    import selectedButton from "../ProfileCard/ProfileCard.svelte";
-    import WorkExperienceMiddleTile from "./WorkExperienceMiddleTile.svelte";
+	import WorkExperienceMiddleTile from "./WorkExperienceMiddleTile.svelte";
 	import WorkExperienceTile from "./WorkExperienceTile.svelte";
+	import ProjectMiddleTile from "./ProjectMiddleTile.svelte";
+	import ProjectTextTile from "./ProjectTextTile.svelte";
+	import AchievementDetailsMiddleTile from "./achievementDetailsMiddleTile.svelte";
+	import AchievementTile from "./AchievementTile.svelte";
+	import CertificationDetailsMiddleTile from "./certificationDetailsMiddleTile.svelte";
+	import CertficationTile from "./CertficationTile.svelte";
 </script>
-<EducationFrame/>
-<CollegeTileText/>
+{#if $clickedBtn ==="Education"}
+    <EducationFrame/>
+    <CollegeTileText/>
+{:else if $clickedBtn === "Work Experience"}
+    <WorkExperienceMiddleTile/>
+    <WorkExperienceTile/>
+{:else if $clickedBtn === "Projects"}
+    <ProjectMiddleTile/>
+    <ProjectTextTile/>
+{:else if $clickedBtn === "Achievements"}
+    <AchievementDetailsMiddleTile/>
+    <AchievementTile/>
+{:else if $clickedBtn === "Certifications"}
+    <CertificationDetailsMiddleTile/>
+    <CertficationTile/>
+{/if}
