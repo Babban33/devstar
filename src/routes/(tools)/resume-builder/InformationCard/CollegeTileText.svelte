8dcf609cc1560358faffecb8a9c99d795144e3b1
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-14 04:25:09 +0530

    update: removed anchor tag

--- src/routes/(tools)/resume-builder/InformationCard/CollegeTileText.svelte
+++ src/routes/(tools)/resume-builder/InformationCard/CollegeTileText.svelte
@@ -3,12 +3,12 @@
     <div class="justify-start px-5 py-5 align-middle">
       <div class="pb-1 font-bold">College Name, City</div>
       <div class="pb-1 text-gray-800">
-        Degree | <a href="#" class="text-gray-800">CGPA:</a>
+        Degree | CGPA:
         <div class="text-gray-500">Start Year - End year</div>
       </div>
     </div>
   </div>
-  <div on:click={collegeForm} class="relative left-0 top-20 -translate-x-1/2 -translate-y-1/2 transform">
+  <div class="relative left-0 top-20 -translate-x-1/2 -translate-y-1/2 transform">
     <button class="flex items-center rounded-xl border-2 border-gray-800 p-1 pl-3 pr-3 font-medium">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 h-4 w-4">
         <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
@@ -17,19 +17,4 @@
     </button>
   </div>
 </div>
-
-<script >
-import {educationStore} from "./eduDetails"
-  
-  import Educationform from "./Educationform.svelte";
-  let isEditing = false
-  function collegeForm(){
-    isEditing = !isEditing;
-  }
-  
-</script>
-
-{#if isEditing}
-<Educationform onSubmit={collegeForm}  onCancel={collegeForm}/>
-{/if}
 
