22f47bc9a655447809924fc4ae5bc5c2f3152929
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-13 22:12:24 +0530

    update: solved merge conflict

--- src/routes/(tools)/resume-builder/InformationCard/projectform.svelte
+++ src/routes/(tools)/resume-builder/InformationCard/projectform.svelte
@@ -57,21 +57,9 @@
         <textarea id="projectdesc" name="projectdesc" class="w-full rounded-xl border border-gray-600 px-3 py-2" rows="2" bind:value="{projects.project1.description1}" placeholder="Enter your description"></textarea>
       </div>
       <div class="mt-6 flex justify-end sm:mt-8">
-        <button type="button" id="closebutton" class="mr-4 px-4 py-2 font-thin text-black">Close</button>
+        <button on:click={closeForm} type="button" id="closebutton" class="mr-4 px-4 py-2 font-thin text-black">Close</button>
         <button type="submit" class="rounded-xl border border-gray-800 bg-gray-900 p-1 px-6 py-2 font-thin text-white">Done</button>
       </div>
     </form>
   </div>
-<<<<<<< HEAD
 </div>
-=======
-  <div class="mt-4 flex-grow sm:mt-8">
-    <label for="projectdesc" class="mb-1 block font-bold text-gray-900">Project Description</label>
-    <textarea id="projectdesc" name="projectdesc" class="w-full rounded-xl border border-gray-600 px-3 py-2" rows="2" bind:value="{projects.project1.description1}" placeholder="Enter your description"></textarea>
-  </div>
-  <div class="mt-6 flex justify-end sm:mt-8">
-    <button on:click={closeForm} type="button" id="closebutton" class="mr-4 px-4 py-2 font-thin text-black">Close</button>
-    <button type="submit" class="rounded-xl border border-gray-800 bg-gray-900 p-1 px-6 py-2 font-thin text-white">Done</button>
-  </div>
-</form>
->>>>>>> 5a21b2722354b2d38ea641b827289588ac7b388f
