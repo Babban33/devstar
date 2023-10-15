57b45e20e3fdfeff13e22c5917f09f7646a69711
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-15 14:11:14 +0530

    update: removed bind values

--- src/routes/(tools)/resume-builder/ProfileCard/profile_form.svelte
+++ src/routes/(tools)/resume-builder/ProfileCard/profile_form.svelte
@@ -10,23 +10,23 @@
     <div class="w-full px-4 sm:w-3/4">
       <div class="mb-4">
         <label for="yourname" class="mb-1 block font-bold text-gray-900">Enter Name</label>
-        <input type="text" id="yourname" name="yourname" class="w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{yourname}" placeholder="Enter your name" />
+        <input type="text" id="yourname" name="yourname" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter your name" />
       </div>
       <div class="mb-4">
         <label for="designation" class="mb-1 block font-bold text-gray-900">Enter Designation</label>
-        <input type="text" id="designation" name="designation" class="w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{designation}" placeholder="Enter your designation" />
+        <input type="text" id="designation" name="designation" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter your designation" />
       </div>
       <div class="mb-4">
         <label for="githublink" class="mb-1 block font-bold text-gray-900">GitHub Link</label>
-        <input type="text" id="githublink" name="githublink" class="w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{githublink}" placeholder="Enter your GitHub link" />
+        <input type="text" id="githublink" name="githublink" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter your GitHub link" />
       </div>
       <div class="mb-4">
         <label for="linkedinlink" class="mb-1 block font-bold text-gray-900">LinkedIn Link</label>
-        <input type="text" id="linkedinlink" name="linkedinlink" class="w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{linkedinlink}" placeholder="Enter your LinkedIn link" />
+        <input type="text" id="linkedinlink" name="linkedinlink" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter your LinkedIn link" />
       </div>
       <div class="mb-4">
         <label for="gmail" class="mb-1 block font-bold text-gray-900">Email Address</label>
-        <input type="text" id="gmail" name="gmail" class="w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{gmail}" placeholder="Enter your email address" />
+        <input type="text" id="gmail" name="gmail" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter your email address" />
       </div>
     </div>
   </div>
