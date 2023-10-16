59845ceaaa787ba9a0046a74c8e961a82809cb24
Dikshit Dhar <dikshitdhar03@gmail.com>
2023-10-16 22:31:17 +0530

    updated form styling

--- src/routes/(tools)/resume-builder/InformationCard/certificationForm.svelte
+++ src/routes/(tools)/resume-builder/InformationCard/certificationForm.svelte
@@ -2,6 +2,7 @@
 // @ts-nocheck
 
 	import { Certificationstore } from "./Certificationstore";
+	let certificates={link :" ", title: " "}
 	
 	/**
 	 * @type {() => void}
@@ -9,18 +10,25 @@
 	export let onSubmit;
 	function formSubmit() {
 		onSubmit();
+		Certificationstore.update(items => {
+      return [...items, {id: $Certificationstore.length, link: certificates.link, title: certificates.title}];
+    });
+	console.log($Certificationstore.length);
 	}
 </script>
 
+
+<div class="form-upper">
+	<div class="form-container">
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <form
 	on:submit={formSubmit}
 	id="certificationform"
-	class="relative flex w-3/4 flex-col rounded-3xl border-2 border-gray-100 p-6 shadow-2xl sm:w-2/3 lg:w-1/2 xl:w-1/3"
+	
 >
-	<div class="mb-7 text-left text-xl font-bold">Certification Details</div>
-	<div class="flex-1">
-		<div class="ml-4 flex-grow-0 sm:ml-0">
+	<div class="text-left text-xl font-bold">Certification Details</div>
+	<div class="flex-grow sm:mt-8">
+		
 			<label for="certificatelink" class="mb-1 block font-bold text-gray-900">
 				Links (Optional)</label
 			>
@@ -29,12 +37,14 @@
 				id="certificatelink"
 				name="certificatelink"
 				class="w-full rounded-xl border border-gray-600 px-3 py-2"
-				bind:value={Certificate.link}
+				bind:value={certificates.link}
 				placeholder="Enter certificate link"
 			/>
-		</div>
+		
 	</div>
-	<div class="mt-4 flex-grow sm:mt-8">
+
+	<div class="flex-1">
+		<div class="ml-4 flex-grow-0 sm:ml-0">
 		<label for="certificateTitle" class="mb-1 block font-bold text-gray-900"
 			>Certificate Title</label
 		>
@@ -43,10 +53,14 @@
 			name="certificateTitle"
 			class="w-full rounded-xl border border-gray-600 px-3 py-2"
 			rows="2"
-			bind:value={Certificate.title}
+			bind:value={certificates.title}
 			placeholder="Enter your description"
+			required
 		/>
 	</div>
+	</div>
+
+	
 	
 	<div class="mt-6 flex justify-end sm:mt-8">
 		<button type="button" id="closebutton" class="mr-4 px-4 py-2 font-thin text-black">Close</button
@@ -57,4 +71,29 @@
 			>Done</button
 		>
 	</div>
-</form>
+ </form>
+	</div>
+</div>
+
+<style>
+
+.form-upper {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+    backdrop-filter: blur(3px);
+  }
+
+  .form-container {
+    background-color: white;
+    max-width: 600px;
+    padding: 20px;
+    border-radius: 10px;
+  }
+  </style>
