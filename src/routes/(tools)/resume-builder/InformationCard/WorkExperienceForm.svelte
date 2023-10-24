83280f12d67cc6eaad3584f2b2bc4312386ffdf2
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-24 10:42:21 +0530

    update : binded store to input fields

--- src/routes/(tools)/resume-builder/InformationCard/WorkExperienceForm.svelte
+++ src/routes/(tools)/resume-builder/InformationCard/WorkExperienceForm.svelte
@@ -49,29 +49,53 @@
       <div class="mb-4 text-left text-xl font-bold">Work Experience</div>
       <div class="py-3">
         <label for="company" class="block mb-1 text-gray-900 font-bold">Company name</label>
-        <input bind:value={experience.company} type="text" id="company" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Ex: Microsoft, Google, Facebook, etc." required>
+        {#if idtoadd > -1}
+          <input bind:value={$WorkExpStore[idtoadd].company_name} type="text" id="company" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Ex: Microsoft, Google, Facebook, etc." required>
+        {:else}
+          <input bind:value={experience.company} type="text" id="company" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Ex: Microsoft, Google, Facebook, etc." required>
+        {/if}
       </div>
       <div class="py-2">
         <label for="job" class="block mb-1 text-gray-900 font-bold">Job role</label>
-        <input bind:value={experience.job} type="text" id="job" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="e.g., Manager, Project Leader, etc." required>
+        {#if idtoadd > -1}
+          <input bind:value={$WorkExpStore[idtoadd].designation} type="text" id="job" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Ex: Software Engineer, Data Analyst, etc." required>
+        {:else}
+          <input bind:value={experience.job} type="text" id="job" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Ex: Software Engineer, Data Analyst, etc." required>
+        {/if}
       </div>
       <div class="py-2">
         <label for="link" class="block mb-1 text-gray-900 font-bold">Point 1</label>
-        <input bind:value={experience.point1} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter first bullet point" required>
+        {#if idtoadd > -1}
+          <input bind:value={$WorkExpStore[idtoadd].discription_1} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter first bullet point" required>
+        {:else}
+          <input bind:value={experience.point1} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter first bullet point" required>
+        {/if}
       </div>
       <div class="py-2">
         <label for="link" class="block mb-1 text-gray-900 font-bold">Point 2</label>
-        <input bind:value={experience.point2} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter second bullet point" required>
+        {#if idtoadd > -1}
+          <input bind:value={$WorkExpStore[idtoadd].discription_2} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter second bullet point" required>
+        {:else}
+          <input bind:value={experience.point2} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter second bullet point" required>
+        {/if}
       </div>
       <div class="p-2">
         <label for="dates" class="block mb-2 text-sm font-bold">Start - End Date</label>
         <div class="flex items-center">
           <div class="relative">
-            <input required bind:value={experience.start} name="start" type="date" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Select start date">
+            {#if idtoadd > -1}
+              <input required bind:value={$WorkExpStore[idtoadd].start_date} name="start" type="date" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Select start date">
+            {:else}
+              <input required bind:value={experience.start} name="start" type="date" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Select start date">
+            {/if}
           </div>
           <span class="mx-4 text-gray-500">to</span>
           <div class="relative">
-            <input required bind:value={experience.end} name="end" type="date" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Select end date">
+            {#if idtoadd > -1}
+              <input required bind:value={$WorkExpStore[idtoadd].end_date} name="end" type="date" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Select end date">
+            {:else}
+              <input required bind:value={experience.end} name="end" type="date" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Select end date">
+            {/if}
           </div>
         </div>
       </div>
