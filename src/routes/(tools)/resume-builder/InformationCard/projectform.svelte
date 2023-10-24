60c4230246841d16da21dcecced70f7276a56152
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-24 10:17:42 +0530

    update : added idforadd prop

--- src/routes/(tools)/resume-builder/InformationCard/projectform.svelte
+++ src/routes/(tools)/resume-builder/InformationCard/projectform.svelte
@@ -8,19 +8,23 @@
     link: "",
   }
   export let onCancel: ()=> void;
+  export let idtoadd: number;
   function formSubmit(){
     onCancel();
-    projectStore.update(items => [
-      ...items,
-      {
-        id: $projectStore.length,
-        projectName: project.name,
-        description1: project.description1,
-        description2: project.description2,
-        projectLink: project.link,
-        tech_used: project.tech_used,
-      }
-    ])
+    if (idtoadd > -1)
+    {
+      projectStore.update(items => [
+        ...items,
+        {
+          id: $projectStore.length,
+          projectName: project.name,
+          description1: project.description1,
+          description2: project.description2,
+          projectLink: project.link,
+          tech_used: project.tech_used,
+        }
+      ]);
+    }
   }
   
 </script>
@@ -53,20 +57,36 @@
       <div class="flex">
         <div class="mr-4 flex-grow sm:mr-8">
           <label for="projectname" class="mb-1 block font-bold text-gray-900">Project Name</label>
-          <input required type="text" id="projectname" name="projectname" class="sm:w-2/ w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{project.name}" placeholder="Enter project name" />
+          {#if idtoadd > -1}
+            <input required type="text" id="projectname" name="projectname" class="sm:w-2/ w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{$projectStore[idtoadd].projectName}" placeholder="Enter project name" />
+          {:else}
+            <input required type="text" id="projectname" name="projectname" class="sm:w-2/ w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{project.name}" placeholder="Enter project name" />
+          {/if}
         </div>
         <div class="ml-4 flex-grow sm:ml-8">
           <label for="projectlink" class="mb-1 block font-bold text-gray-900">Project Link</label>
-          <input required type="text" id="projectlink" name="projectlink" class="w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{project.link}" placeholder="Enter project link" />
+          {#if idtoadd > -1}
+            <input required type="text" id="projectlink" name="projectlink" class="w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{$projectStore[idtoadd].projectLink}" placeholder="Enter project link" />
+          {:else}
+            <input required type="text" id="projectlink" name="projectlink" class="w-full rounded-xl border border-gray-600 px-3 py-2" bind:value="{project.link}" placeholder="Enter project link" />
+          {/if}
         </div>
       </div>
       <div class="flex-grow sm:mt-2">
         <label for="projectdesc" class="mb-1 block font-bold text-gray-900">Point 1</label>
-        <input bind:value={project.description1} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter first bullet point" required>
+        {#if idtoadd > -1}
+          <input bind:value={$projectStore[idtoadd].description1} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter first bullet point" required>
+        {:else}
+          <input bind:value={project.description1} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter first bullet point" required>
+        {/if}
       </div>
       <div class="flex-grow sm:mt-2">
         <label for="projectdesc" class="mb-1 block font-bold text-gray-900">Point 2</label>
-        <input bind:value={project.description2} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter second bullet point" required>
+        {#if idtoadd > -1}
+          <input bind:value={$projectStore[idtoadd].description2} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter second bullet point" required>
+        {:else}
+          <input bind:value={project.description2} type="text" id="link" class="w-full rounded-xl border border-gray-600 px-3 py-2" placeholder="Enter second bullet point" required>
+        {/if}
       </div>
       <div class="mt-6 flex justify-end sm:mt-8">
         <button on:click={onCancel} type="button" id="closebutton" class="mr-4 px-4 py-2 font-thin text-black">Close</button>
