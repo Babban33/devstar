4cd54bba67a69d953188ae65f2974a33debf54dc
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-24 06:58:33 +0530

    update : removed else if block

--- src/routes/(tools)/resume-builder/InformationCard/AchievementsForm.svelte
+++ src/routes/(tools)/resume-builder/InformationCard/AchievementsForm.svelte
@@ -10,24 +10,24 @@
         return [...items, {id: $achievementStore.length, achievementName: achievements.achievementName, achievementLink: achievements.achievementLink}];
       });
     }
-    else if(idtoadd > -1){
-      $achievementStore[idtoadd].achievementName = achievements.achievementName;
-      $achievementStore[idtoadd].achievementLink = achievements.achievementLink;
-    }
+    // else if(idtoadd > -1){
+    //   $achievementStore[idtoadd].achievementName = achievements.achievementName;
+    //   $achievementStore[idtoadd].achievementLink = achievements.achievementLink;
+    // }
     console.log($achievementStore.length);
   }
 </script>
 <div class="form-upper">
   <div class="form-container">
-    <form on:submit={formSubmit} id="projectform" >
+    <form on:submit|preventDefault={formSubmit} id="projectform" >
       <div class="text-left text-xl font-bold">Achievements Details</div>
       
       <div class="flex-grow sm:mt-8">
         <label for="projectdesc" class="mb-1 block font-bold text-gray-900">Description</label>
         {#if idtoadd >-1}
-          <textarea id="projectdesc" name="projectdesc" class="w-full rounded-xl border border-gray-600 px-3 py-2" rows="2" bind:value="{$achievementStore[idtoadd].achievementName}" placeholder="Enter your description"></textarea>
+          <textarea id="projectdesc" name="projectdesc" class="w-full rounded-xl border border-gray-600 px-3 py-2" rows="2" bind:value="{$achievementStore[idtoadd].achievementName}" placeholder="Enter your description" required></textarea>
         {:else}
-          <textarea id="projectdesc" name="projectdesc" class="w-full rounded-xl border border-gray-600 px-3 py-2" rows="2" bind:value="{achievements.achievementName}" placeholder="Enter your description"></textarea>
+          <textarea id="projectdesc" name="projectdesc" class="w-full rounded-xl border border-gray-600 px-3 py-2" rows="2" bind:value="{achievements.achievementName}" placeholder="Enter your description" required></textarea>
         {/if}
       </div>
       <div class="flex-1">
