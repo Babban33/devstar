dc92313eec17c31915bcd58ccdc2e29cae8c1bc5
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-10-28 20:35:01 +0530

    update : made education section dynamic and removed hr dotted line

--- src/routes/(tools)/resume-builder/templates/Template.svelte
+++ src/routes/(tools)/resume-builder/templates/Template.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { WorkExpStore } from "../InformationCard/workExp";
-import { profileStore } from "../ProfileCard/ProfileStore";
+  import { EducationStore } from "../InformationCard/EducationStore";
+import { WorkExpStore } from "../InformationCard/workExp";
+  import { profileStore } from "../ProfileCard/ProfileStore";
 </script>
 <style>
     .dotted-line {
@@ -62,36 +63,57 @@ import { profileStore } from "../ProfileCard/ProfileStore";
                 </ul>
             </div> -->
             {#each $WorkExpStore as {id} (id)}
-                <div class="mb-4">
-                    <div class="flex justify-between">
-                        <h2 class="text-xl font-semibold">{$WorkExpStore[id].designation}</h2>
-                        <span>
-                            <p class="flex gap-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 solid">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
-                                </svg>
-                                {$WorkExpStore[id].start_date} - {$WorkExpStore[id].end_date}
-                            </p>
-                        </span>
-                    </div>
-                    <p class="text-blue-500">{$WorkExpStore[id].company_name}</p>
-                    <ul class="list-disc pl-6">
-                        <li>{$WorkExpStore[id].discription_1}</li>
-                        <li>{$WorkExpStore[id].discription_2}</li>
-                    </ul>
+              <div class="mb-4">
+                <div class="flex justify-between">
+                  <h2 class="text-xl font-semibold">{$WorkExpStore[id].designation}</h2>
+                  <span>
+                    <p class="flex gap-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 solid">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                      </svg>
+                      {$WorkExpStore[id].start_date} - {$WorkExpStore[id].end_date}
+                    </p>
+                  </span>
                 </div>
+                <p class="text-blue-500">{$WorkExpStore[id].company_name}</p>
+                <ul class="list-disc pl-6">
+                  <li>{$WorkExpStore[id].discription_1}</li>
+                  <li>{$WorkExpStore[id].discription_2}</li>
+                </ul>
+              </div>
             {/each}
-            {#if $WorkExpStore.length > 1}
-                <hr class="dotted-line mb-2" />
-            {/if}
+            <!-- {#if $WorkExpStore.length > 1 && $WorkExpStore.length < 3}
+              <hr class="dotted-line mb-2" />
+            {/if} -->
         </section>
     {/if}
-  
-    <p class="mt-6 text-xl font-bold">EDUCATION</p>
-    <hr class="bor-line mb-4" />
-    <h2 class="text-xl font-semibold">Master Of Marketing Management [MMM]</h2>
-    <p class="text-blue-500">Drouln S.A.R.L</p>
-    <p class="ml-auto"><i class="fas fa-calendar mr-2"></i>2007 - 2009 <i class="fas fa-map-marker-alt ml-4 mr-2"></i>New York, USA</p>
+    {#if $EducationStore.length > 0}
+      <p class="mt-6 text-xl font-bold">EDUCATION</p>
+      <hr class="bor-line mb-4" />
+      <!-- <h2 class="text-xl font-semibold">Master Of Marketing Management [MMM]</h2>
+      <p class="text-blue-500">Drouln S.A.R.L</p>
+       -->
+      {#each $EducationStore as {id} (id)}
+        <div class="mb-4">
+          <div class="flex justify-between">
+            <h2 class="text-xl font-semibold">{$EducationStore[id].degree}</h2>
+            <span>
+              <p class="flex gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 solid">                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                </svg>
+                {$EducationStore[id].start_year} - {$EducationStore[id].end_year}
+              </p>
+            </span>
+          </div>
+          <p class="text-blue-500">{$EducationStore[id].college_name}</p>
+          <p>CGPA: {$EducationStore[id].CGPA}</p>
+        </div>
+      {/each}
+      <!-- {#if $EducationStore.length > 1 || $EducationStore.length < 3}
+        <hr class="dotted-line mb-2" />
+      {/if} -->
+    {/if}
+    
   
     <p class="mt-6 text-xl font-bold">PROJECTS</p>
     <hr class="bor-line mb-4" />
